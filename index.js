b68b197bfb994ac33787cc4f9e77928bba729328
Fuad <fuadquliyev9977@gmail.com>
2024-03-31 19:09:20 -0800

    new project first codes

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+
+const myArr = [[1,2],[3,4],[5,6]];
+
+const newArr = myArr.flat(2);
+document.getElementById("demo").innerHTML = newArr;
+
+
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("555" < false) 
+
+// console.log(false < "salam") //her ikisi niye false verir? 
+// console.log(false > "salam")
+
